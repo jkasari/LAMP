3fddbaa128908a79e0526589cc3c5961aa85d2cc
jkasari <jkasari@outlook.com>
2022-10-17 18:55:57 -0700

    Buttons are done and working

--- LAMPbutton.cpp
+++ LAMPbutton.cpp
@@ -1,13 +1,18 @@
+#include <Arduino.h>
 #include "LAMPbutton.h"
 
 Button::Button(uint8_t Port) { this->Port = Port; }
 
+void Button::begin() {
+    pinMode(Port, INPUT_PULLUP);
+}
+
 uint32_t Button::hasBeenPressed() {
     uint32_t value = 0;
-    if (isPressed) {
+    if (isPressed()) {
         Count++;
     }
-    if (!isPressed && Count != 0) {
+    if (!isPressed() && Count > 0) {
         value = Count;
         Count = 0;
     }
@@ -15,5 +20,5 @@ uint32_t Button::hasBeenPressed() {
 }
 
 bool Button::isPressed() {
-    return digitalRead(Port) == LOW;
+    return digitalRead(Port) == HIGH;
 }
